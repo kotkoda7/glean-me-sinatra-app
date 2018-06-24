27185815fa0bafdf7b38f6b551abcebd9b7537c2
kotkoda7 <christopherrobincox@Szilvias-MacBook-Air.local>
2018-06-24 07:56:55 -0700

    new seed, edit, new views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,32 +1,52 @@
 class LocationsController < ApplicationController
 
-	get "/locations" do 
-		authenticate_user
-		@locations = Location.all
-		erb :'locations/index'
-	end
+  get "/locations" do
+    authenticate_user
+    @locations = Location.all
+    erb :'locations/index'
+  end
 
-	get '/locations/new' do
-		authenticate_user
-		@locations = Location.all
-		@edibles = Edible.all
-		erb :'/locations/new'
-	end
+  get "/locations/new" do
+    authenticate_user
+    @locations = Location.all
+    @edibles = Edible.all
+    @error_message = params[:error]
+    erb :'locations/new'
+  end
 
-	get "/locations/:id/edit" do
-    	authenticate_user
-   		@location = Location.find(params[:id])
-    	erb :'locations/edit'
+  get "/locations/:id/edit" do
+    authenticate_user
+    @location = Location.find(params[:id])
+    @edibles = Edible.all
+    erb :'locations/edit'
+  end
+
+  post "/locations/:id" do
+    authenticate_user
+    @location = Location.find(params[:id])
+    unless Location.valid_params?(params)
+      redirect "/locations/#{@location.id}/edit?error=invalid location"
     end
+    @location.update(params.select{|k| k=="address"})
+    redirect "/locations/#{@location.id}"
+  end
 
-	get '/locations/:id' do
-		authenticate_user
-		@location = Location.find_by_id(params[:id])
-		if @location
-			erb :'/locations/show'
-		else
-			redirect '/locations'
-		end
+  get "/locations/:id" do
+    authenticate_user
+    @location = Location.find(params[:id])
+    erb :'locaitons/show'
+  end
+
+
+post "/locations" do
+	authenticate_user
+
+	unless Location.valid_params?(params)
+  		redirect "/locations/new?error=invalid location"
 	end
+
+	Location.create(params)
+	redirect "/locations"
+end
 
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -3,5 +3,9 @@ class Location < ActiveRecord::Base
 	belongs_to :user
 	has_many :edibles
 
+	def self.valid_params?(params)
+    	return !params[:address].empty? || (!params[:lat].empty? && !params[:lng].empty?)
+  	end
+ 
 	
 end
