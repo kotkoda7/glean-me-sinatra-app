3dd8d44d25669e278725fa577a1070ce763975b6
kotkoda7 <christopherrobincox@Szilvias-MacBook-Air.local>
2018-06-27 11:19:14 -0700

    fixing locations controller and erbs

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,6 +6,7 @@ class LocationsController < ApplicationController
     erb :'locations/index'
   end
 
+=begin
   post "/locations" do
     authenticate_user
 
@@ -16,6 +17,7 @@ class LocationsController < ApplicationController
     Location.create(params[:location])
     redirect "/locations"
   end
+=end
 
   get "/locations/new" do
     authenticate_user
@@ -24,16 +26,27 @@ class LocationsController < ApplicationController
     erb :'locations/new'
   end
 
-  get "/locations/:id/edit" do
+post '/locations/new' do
     authenticate_user
-    @location = Location.find(params[:id])
-    @locations = Location.all
-    @edible = Edible.find(params[:id])
-    @edibles = Edible.all
-    erb :'locations/edit'
-  end
+    params[:location][:edible] = params[:location][:selected_edible] if !params[:location][:selected_edible].empty? 
+    if !params[:location][:address].empty? && !params[:location][:edible].empty?
+      edible = Edible.find_or_create_by(name: params[:location][:edible])
+      @location = Location.create(address: params[:location][:address], 
+        lat: params[:location][:lat], 
+        lng: params[:location][:lng],
+        loc_type: params[:location][:loc_type],
+        description: params[:location][:description],
+        user_id: current_user.id)
 
-   post "/locations/:id" do
+      #flash[:message] = "The location is successfully added."
+      redirect "/locations/#{@location.id}"
+    else
+      #flash[:message] = "Both fields must be filled in. Please complete the form."
+      redirect '/locations/new'
+  end
+end
+
+  post "/locations/:id" do
     authenticate_user
     @location = Location.find(params[:id])
     unless Location.valid_params?(params)
@@ -41,6 +54,15 @@ class LocationsController < ApplicationController
     end
     @location.update(params.select{|k| k=="address" || k=="lat" || k=="lng" || k=="description" || k=="edible_id"})
     redirect "/locations/#{@location.id}"
+  end
+
+    get "/locations/:id/edit" do
+    authenticate_user
+    @location = Location.find(params[:id])
+    @locations = Location.all
+    @edible = Edible.find(params[:id])
+    @edibles = Edible.all
+    erb :'locations/edit'
   end
 
   get "/locations/:id" do
@@ -62,9 +84,24 @@ class LocationsController < ApplicationController
     if !params[:edible_name].empty?
       Edible.create(name: params[:edible_name], location: @location, user: current_user)
 
-      redirect "/locations/#{@location.id}"
+      redirect to "/locations/#{@location.id}"
     else
-      redirect "/locations/#{@location.id}/add_edible"
+      redirect to "/locations/#{@location.id}/add_edible"
+    end
+  end
+
+get '/locations/:id/delete' do
+    authenticate_user
+    @location = Location.find_by_id(params[:id])
+    @edible = @location.edible_id
+    if @location.user == current_user
+      @location.destroy
+
+      #flash[:message] = "The location is successfully deleted."
+      redirect "/edibles/#{@location.id}"
+    else
+      #flash[:message] = "You cannot delete another user's location."
+      redirect to "/locations/#{@location.id}"
     end
   end
 
